346c84ab598a78cf17c2ef5f47c4b7d7c057529d
saksham wane <wanesaksham@gmail.com>
2021-05-19 22:37:25 +0530

    News app

---
+++ app/src/main/java/com/example/svpnews/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.svpnews
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+
+
+
+class MainActivity : AppCompatActivity(), NewsItemClicked {
+
+    private lateinit var mAdapter: NewsListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        recycler_view.adapter = mAdapter
+
+    }
+
+    // url = https://newsapi.org/v2/top-headlines?country=in&apiKey=7e6fff5b28824178b6b91e0ce525f081
+
+    private fun fetchData() {
+        val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=7e6fff5b28824178b6b91e0ce525f081"
+        val jsonObjectRequest = object : JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+
+                }
+
+                mAdapter.updateNews(newsArray)
+
+            },
+            Response.ErrorListener {
+
+            }
+
+        ) {
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
+
+        }
+
+            MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+
+    }
+
+    override fun onItemClicked(item: News) {
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(item.url))
+
+    }
+}
---
+++ app/src/main/java/com/example/svpnews/News.kt
@@ -0,0 +1,8 @@
+package com.example.svpnews
+
+data class News (
+   val title: String,
+   val author: String,
+   val url: String,
+   val imageUrl: String
+    )
